7bbbbaa1bdc1801658ce0555fbc8272c8b438c0f
Hitesh Moudgil <200378502@student.georgianc.on.ca>
2025-02-12 17:00:46 -0500

    added Base Test

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,46 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.time.Duration;
+
+public class BaseTest {
+    AndroidDriver driver;
+    AppiumDriverLocalService service;
+
+    @Test
+    public void configureAppium() throws IOException, InterruptedException {
+        // Start the Appium server automatically
+        service = new AppiumServiceBuilder().withAppiumJS(new File("/Users/hiteshmoudgil/node_modules/appium/build/lib/main.js"))
+                .withIPAddress("127.0.0.1")
+                .usingPort(4723)
+                .build();
+        service.start();
+
+        // Set the capabilities
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setDeviceName("TestingEmulator");
+        options.setAppPackage("com.veeva.vault.mobile");
+        options.setAppActivity("veeva.vault.mobile.MainActivity");
+        Thread.sleep(5000);
+
+        // Initialize the driver
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), options);
+
+        // This is to delay the moving between screens
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if(driver!=null){
+            driver.quit();
+        }
+    }
+}
